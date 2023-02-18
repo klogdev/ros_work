a33478e37a2ee1dad4645bcf65bddcccc9fdca1d
Charlie <klog.dev.team@gmail.com>
2023-02-17 16:54:47 -0800

    ros tuotorials of talker and listener.

---
+++ src/beginner_tutorials/src/listener.cpp
@@ -0,0 +1,14 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void chatter_callback(const std_msgs::String::ConstPtr& msg) {
+  std::cout << "I heard: " << msg->data.c_str() << std::endl;
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatter_callback);
+  ros::spin();
+  return 0;
+}
